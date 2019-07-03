c142a15435c36e9319e39368505e764045b4aaa2
kv-2 <str_token@mail.ru>
2019-07-03 19:53:21 +0300

    add shallow nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  # nesting comments resources by posts
+  # shalliw nesting 
   resources :posts do
-    resources :comments
+    resources :comments, only: [:index, :new, :create]
   end
+
+  resources :comments, except: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
